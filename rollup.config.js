ad3a8555e95c7de1fb25f4881af80598e60d8452
ngoviet <ngoviet@users.noreply.github.com>
2025-11-30 19:13:56 +0700

    Fix: Reorder rollup plugins - TypeScript before nodeResolve, update hacs.json

--- rollup.config.js
+++ rollup.config.js
@@ -43,6 +43,13 @@ const plugins = [
   }),
   svgo(),
   image({ exclude: '**/*.svg' }),
+  typescript({
+    sourceMap: dev,
+    inlineSources: dev,
+    exclude: ['dist/**', 'tests/**/*.test.ts'],
+    tsconfig: 'tsconfig.json',
+    resolveJsonModule: true,
+  }),
   nodeResolve({
     browser: true,
     preferBuiltins: false,
@@ -51,13 +58,6 @@ const plugins = [
   commonjs({
     include: 'node_modules/**',
     sourceMap: false,
-  }),
-  typescript({
-    sourceMap: dev,
-    inlineSources: dev,
-    exclude: ['dist/**', 'tests/**/*.test.ts'],
-    tsconfig: 'tsconfig.json',
-    resolveJsonModule: true,
   }),
   json({ exclude: ['package.json', 'node_modules/**/package.json'] }),
   replace({
